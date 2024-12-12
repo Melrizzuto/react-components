159a38c3f1af3ca2c10a7de84053a87bcf8ae887
Melrizzuto <melrizzuto4@gmail.com>
2024-12-12 17:15:18 +0100

    changed style to card and button in Card.module.css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
-import styles from './Card.module.css';
+import style from './Card.module.css'
 
 function Card({ title, description, img }) {
     return (
-        <div className={`card m-1 ${styles.card}`}>
+        <div className={`card m-1 ${style.card}`} >
             <img src={img} className="card-img-top" alt={title} />
             <div className="card-body">
-                <h5 className={`${styles.cardTitle}`}>{title}</h5>
+                <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href="#" className={`btn ${styles.cardButton}`}>Leggi di più</a>
+                <a href="#" className={style.cardButton}>Leggi di più</a>
             </div>
-        </div>
+        </ div >
     );
 }
 
