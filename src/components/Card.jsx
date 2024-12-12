9a09fda03f7e7ae0b2fcaa739d5d36bda1dd68af
Melrizzuto <melrizzuto4@gmail.com>
2024-12-12 17:52:22 +0100

    added style to img Card jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
-import style from './Card.module.css'
+import style from './Card.module.css';
 
 function Card({ title, description, img }) {
     return (
-        <div className={`card m-1 ${style.card}`} >
-            <img src={img} className="card-img-top" alt={title} />
+        <div className={`card m-1 ${style.card}`}>
+            <img src={img} className={`card-img-top ${style.cardImg}`} alt={title} />
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
                 <a href="#" className={style.cardButton}>Leggi di più</a>
             </div>
-        </ div >
+        </div>
     );
 }
 
