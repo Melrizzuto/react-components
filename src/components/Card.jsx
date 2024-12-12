421ee9cede78a809b8fc0399a4dd63ff9c91e92b
Melrizzuto <melrizzuto4@gmail.com>
2024-12-12 15:43:50 +0100

    created Card components and added tag in App jsx

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+function Card() {
+    return (
+        <main className="d-flex justify-content-center align-content-center">
+            <div className="card" style={{ width: "30rem" }}>
+                <img src="https://via.placeholder.com/600x400" className="card-img-top" alt="Placeholder" />
+                <div className="card-body">
+                    <h5 className="card-title">Titolo del post</h5>
+                    <p className="card-text">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum, eveniet. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, dolorem aliquam magni deserunt
+                    </p>
+                    <a href="#" className="btn btn-primary">Leggi di più</a>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Card;
+
+
